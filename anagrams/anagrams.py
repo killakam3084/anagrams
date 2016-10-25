61715e81372faf7df098d6ec8ee4636496649351
Cameron Rison <cameron.rison@utexas.edu>
2016-10-24 22:43:58 -0500

    Finishing up

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -1,60 +1,68 @@
-from memory_profiler import profile
-from tweet import Tweet
-import os
-import ijson
+#!/usr/bin/env python
+
 import argparse
+import ijson.backends.yajl2_cffi
+import time
+from memory_profiler import profile
 
-MIN_WORD_SIZE = 2  # min size of a word in the output
+
+class Tweet:
+    """
+    A representation of a Tweet
+    """
+    def __init__(self, tweet_id, original_tweet_text, tweet_text):
+
+        self.id = tweet_id
+        self.orig_text = original_tweet_text
+        self.text = tweet_text
 
 
-# Tries are crazy cool. Many thanks to Foglebird on S.O.
-class Node(object):
-    def __init__(self, letter='', final=False, depth=0):
-        self.letter = letter
-        self.final = final
-        self.depth = depth
-        self.children = {}
+def permute_words(words):
+    """
+    Takes in a string of words and finds all of the permutations of
+    the input words
+    :param words: a space delimited string
+    :return: either a set if there is more than one word to permute
+             an empty set otherwise
+    """
 
-    def add(self, letters):
-        node = self
-        for index, letter in enumerate(letters):
-            if letter not in node.children:
-                node.children[letter] = Node(letter, index == len(letters) - 1,
-                                             index + 1)
-            node = node.children[letter]
+    words_list = words.split(' ')
+    if len(words_list) > 1:
+        used_list = [False for i in range(len(words_list))]
+        res = ''
+        res_set = set()
+        permute(words_list, used_list, res, 0, res_set)
+        return res_set
+    else:
+        return set()
 
-    def anagram(self, letters):
-        tiles = {}
-        for letter in letters:
-            tiles[letter] = tiles.get(letter, 0) + 1
-        min_length = len(letters)
-        return self._anagram(tiles, [], self, min_length)
 
-    def _anagram(self, tiles, path, root, min_length):
-        if self.final and self.depth >= MIN_WORD_SIZE:
-            word = ''.join(path)
-            length = len(word.replace(' ', ''))
-            if length >= min_length:
-                yield word
-            path.append(' ')
-            for word in root._anagram(tiles, path, root, min_length):
-                yield word
-            path.pop()
-        for letter, node in self.children.iteritems():
-            count = tiles.get(letter, 0)
-            if count == 0:
-                continue
-            tiles[letter] = count - 1
-            path.append(letter)
-            for word in node._anagram(tiles, path, root, min_length):
-                yield word
-            path.pop()
-            tiles[letter] = count
+def permute(words_list, used_list, res, level, res_list):
+    """
+
+    :param words_list: The list of word tokens
+    :param used_list: Keeps track of which positions have been permuted
+    :param res: the result string
+    :param level: permuting each word through each possible position
+    :param res_list: the resulting list of all permutations
+    :return: recursive call
+    """
+    if level == len(words_list) and res:
+        res_list.add(''.join(res.strip().split()))
+        return
+    for i in range(len(words_list)):
+        if used_list[i]:
+            continue
+        else:
+            used_list[i] = True
+            permute(words_list, used_list, res + ' ' + words_list[i], level + 1,
+                    res_list)
+            used_list[i] = False
 
 
 def parse_args():
     """
-        Method to parse commandline arguments of serving size and filter items
+        Method to parse commandline argument of input file.
 
         :return: return nothing
     """
@@ -63,12 +71,8 @@ def parse_args():
         description='A vanilla program to detect anagrams in tweet json',
         epilog='Certainly this isn\'t how Anagramatron does it')
     parser.add_argument('input_file',
-                        type=argparse.FileType('rt'),
+                        type=argparse.FileType('r'),
                         help='The input file of tweets.'
-                        )
-    parser.add_argument('dictionary_file',
-                        type=argparse.FileType('rt'),
-                        help='The dictionary to construct anagrams.'
                         )
     try:
         global args
@@ -77,56 +81,68 @@ def parse_args():
         parser.error(str(msg))
 
 
-def load_dictionary(f):
-    result = Node()
-    for line in f:
-        word = line.strip().lower()
-        result.add(word)
-    return result
-
-
-# @profile
+@profile
 def main():
+    """
+    Loads the input file iterates through it prints matching anagram tweets
+    to std out
+    :return: nothing
+    """
     parse_args()
-
     global args
-    dict_fn = args.dictionary_file
-    print 'Loading word list.\n'
-    words = load_dictionary(dict_fn)
-
-    input_file = args.input_file
-    tweet_obj_dict = {}
-    parser = ijson.parse(input_file)
     tweet_id = ""
     tweet_text = ""
-    for prefix, event, value in parser:
-        if prefix == 'tweets.item.id':
-            tweet_id = str(value)
-        elif prefix == 'tweets.item.text':
-            tweet_text = str(value)
 
-        if tweet_id and tweet_text and tweet_text not in tweet_obj_dict:
-            for t, tweet_obj in tweet_obj_dict.iteritems():
-                if tweet_text in tweet_obj.anagram_set:
-                    print "Original Tweet:::::id:{0} text:\"{1}\"".format(
-                        tweet_obj.id,
-                        tweet_obj.text)
-                    print "Anagram  Tweet:::::id:{0} text:\"{1}\"\n".format(
-                        tweet_id, tweet_text)
+    with open(args.input_file.name, 'r') as input_file:
+        parser = ijson.parse(input_file)
 
-                break
-            tweet_text = ''.join(
-                ch for ch in tweet_text.strip().lower() if
-                ch.isalpha())
-            if not tweet_text:
-                break
-            tweet_set = set()
-            for word in words.anagram(tweet_text):
-                tweet_set.add(word)
-            tweet_obj = Tweet(tweet_id, tweet_text, tweet_set)
-            tweet_obj_dict[tweet_text] = tweet_obj
-            tweet_text = ""
-            tweet_id = ""
+        tweet_obj_dict = {}
+        print "Beginning to parse tweets in..."
+        time_var = 5
+        for i in range(time_var):
+            print str(time_var)
+            time.sleep(1)
+            time_var -= 1
+
+        for prefix, event, value in parser:
+            if prefix == 'tweets.item.id':
+                tweet_id = str(value)
+            if prefix == 'tweets.item.text':
+                original_text = str(value)
+                tweet_text = "".join(original_text.split())
+            if tweet_id and original_text:
+                tweet_key = (''.join(
+                    ch for ch in sorted(tweet_text.strip().lower()) if
+                    ch.isalpha()))
+                if tweet_key in tweet_obj_dict:
+                    if tweet_text != tweet_obj_dict[tweet_key].text:
+                        permutations = permute_words(
+                            tweet_obj_dict[tweet_key].orig_text)
+                        if tweet_text not in permutations:
+                            print
+                            print "*****ANAGRAMS FOUND!*****"
+                            print "Original Tweet   id:{0} text:{1}".format(
+                                tweet_obj_dict[tweet_key].id,
+                                tweet_obj_dict[tweet_key].orig_text)
+                            del tweet_obj_dict[tweet_key]
+                            print "Anagram  Tweet   id:{0} text:{1}\n".format(
+                                tweet_id, original_text)
+                    else:
+                        '''
+                        print
+                        print "*****DUPLICATE TWEETS FOUND!*****"
+                        print "Original Tweet   id:{0} text:{1}".format(
+                            tweet_obj_dict[tweet_key].id,
+                            tweet_obj_dict[tweet_key].orig_text)
+                        print "Duplicate  Tweet   id:{0} text:{1}\n".format(
+                            tweet_id, original_text)
+                            '''
+                else:
+                    tweet_obj = Tweet(tweet_id, original_text, tweet_text)
+                    tweet_obj_dict[tweet_key] = tweet_obj
+                print "Processed Tweet: " + "id:" + tweet_id + "    " + "text: " + original_text
+                tweet_text = ""
+                tweet_id = ""
 
 
 if __name__ == '__main__':
---
+++ anagrams/make_tweet_json.py
@@ -0,0 +1,74 @@
+#!/usr/local/env python
+import json
+import random
+import argparse
+
+
+def parse_args():
+    """
+        Method to parse commandline arguments of serving size and filter items
+
+        :return: return nothing
+    """
+    global parser
+    parser = argparse.ArgumentParser(
+        description='A program to make dummy tweet json for anagrams'
+    )
+    parser.add_argument('output_file',
+                        help='A output file to write tweet data to.'
+                        )
+    parser.add_argument('dictionary_file',
+                        type=argparse.FileType('r'),
+                        help='The dictionary to construct anagrams.'
+                        )
+    parser.add_argument('-l, word-length', type=int,
+                        help='The dictionary to construct anagrams.',
+                        dest='word_length'
+                        )
+    parser.add_argument('-n, number-of-tweets', type=int,
+                        help='The dictionary to construct anagrams.',
+                        dest='num_tweets'
+                        )
+    try:
+        global args
+        args = parser.parse_args()
+    except IOError, msg:
+        parser.error(str(msg))
+
+
+def make_word_string(word_list):
+    """
+    Constructs a string of text from the list of possible words constructed
+    from the read in dictionary file
+    :param word_list: The list of possible words
+    :return: returns a leading and trailing striped string of text
+    """
+    word_str = ''
+    for i in range(random.randint(1, args.word_length)):
+        word_str += " " + word_list[random.randint(0, len(word_list) - 1)]
+    return word_str.strip()
+
+
+def generate_tweet_data():
+    """
+    Create the tweet json object and write it to file
+    :return: return none
+    """
+    global args
+    word_list = ([line.strip() for line in args.dictionary_file if
+                  len(line.strip()) <= args.word_length])
+    word_list.sort()
+    tweet_obj = {
+        'tweets': [{'id': str(i), 'text': make_word_string(word_list)} for i in
+                   range(args.num_tweets)]}
+    with open(args.output_file, 'w') as outfile:
+        json.dump(tweet_obj, outfile, indent=4)
+
+
+def main():
+    parse_args()
+    generate_tweet_data()
+
+
+if __name__ == '__main__':
+    main()
